e13aa90c0fa3ca234b816f7479d06ab93f4ce0e5
Amit Mahadik <amit.mahadik@gmail.com>
2025-10-04 10:03:44 -0700

    Initial Commit

---
+++ SommAI/SommAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SommAIApp.swift
+//  SommAI
+//
+//  Created by Mahadik, Amit on 10/4/25.
+//
+
+import SwiftUI
+
+@main
+struct SommAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
